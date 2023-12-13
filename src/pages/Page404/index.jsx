dc8239b79cab6905bcbf1fae0fbb59b2223726be
Mariana da Silva Lara <marislaradev@outlook.com>
2023-12-12 21:57:55 -0300

    style(responsive): improve responsiveness across multiple devices

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,14 +2,14 @@ import styles from './Page404.module.css'
 
 function Page404() {
     return (
-        <>
-            <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
-            <div className={styles.textos}>
-                <span className={styles.texto_grande}>404</span> <br />
-                <strong className={styles.texto_vermelho}>Página não localizada!</strong>
-            </div>
-        </>
-    )
+            <section className={styles.page404}>
+                <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
+                <div className={styles.textos}>
+                    <span className={styles.texto_grande}>404</span> <br />
+                    <strong className={styles.texto_vermelho}>Página não localizada!</strong>
+                </div>
+            </section>
+        )
 }
 
 export default Page404
