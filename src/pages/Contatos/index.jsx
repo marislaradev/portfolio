7971f44c8761800f0297f4ae52a433b56ac2017e
Mariana da Silva Lara <marislaradev@outlook.com>
2024-03-15 13:59:55 -0300

    feat: add image to contacts page and apply hover effect on contact icons

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,45 +1,55 @@
-import styles from './Contatos.module.css'
+import styles from "./Contatos.module.css";
 import { GoMail } from "react-icons/go";
-import { BsInstagram, BsYoutube, BsGithub, BsLinkedin,  BsWhatsapp  } from "react-icons/bs";
+import {
+  BsInstagram,
+  BsYoutube,
+  BsGithub,
+  BsLinkedin,
+  BsWhatsapp,
+} from "react-icons/bs";
 
 function Contatos() {
-    return (
-        <> 
-            <section className={styles.contatos}>
-            <h2>Contatos</h2>
-            <h3>Entre em contato</h3>
-            <p>Para que possamos conversar mais sobre.</p>
+  return (
+    <>
+      <section className={styles.contatos}>
+        <h2>Contatos</h2>
 
-            <div className={styles.icones}>
+        <h3>Entre em contato comigo</h3>
+        <div className={styles.icones}>
+          <a
+            href="https://github.com/marislaradev"
+            target="_blank"
+            rel="noopenner norefferer"
+          >
+            <BsGithub className={styles.icone} />
+          </a>
 
-            {/* <a href="mailto:marislaradev@outlook.com" target="_blank" rel='noopenner norefferer'>
-                <GoMail className={styles.icone} />
-            </a> */}
+          <a
+            href="https://www.linkedin.com/in/mariana-da-silva-lara/"
+            target="_blank"
+            rel="noopenner norefferer"
+          >
+            <BsLinkedin className={styles.icone} />
+          </a>
 
-            {/* <a href="#" target="_blank" rel='noopenner norefferer'>
-                <BsInstagram className={styles.icone} />
-            </a> */}
-
-            {/* <a href="#" target="_blank" rel='noopenner norefferer'>
-                <BsYoutube className={styles.icone} />
-            </a> */}
-
-            <a href="https://github.com/marislaradev" target="_blank" rel='noopenner norefferer'>
-                <BsGithub className={styles.icone} />
-            </a>
-
-            <a href="https://www.linkedin.com/in/mariana-da-silva-lara/" target="_blank" rel='noopenner norefferer'>
-                <BsLinkedin className={styles.icone} />
-            </a>
-
-            <a href="https://wa.me/554591414654" target="_blank" rel='noopenner norefferer'>
-                <BsWhatsapp className={styles.icone} />
-            </a> 
-
-            </div>
-            </section>
-        </>
-    )
+          <a
+            href="https://wa.me/554591414654"
+            target="_blank"
+            rel="noopenner norefferer"
+          >
+            <BsWhatsapp className={styles.icone} />
+          </a>
+        </div>
+        <figure>
+          <img
+            className={styles.img_contatos}
+            src="/personal-information.svg"
+            alt="Imagem de contatos"
+          ></img>
+        </figure>
+      </section>
+    </>
+  );
 }
 
-export default Contatos
+export default Contatos;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,4 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom';
-import Container from '../../components/Container';
 import styles from './Home.module.css'
 
 function Home() {
